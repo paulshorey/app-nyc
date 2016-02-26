056f8f48a1cdbaa4a8645a15398d8268bf8e00ad
paulshorey <ps@artspaces.net>
2016-02-25 23:23:54 -0500

    good

--- config/consts.js
+++ config/consts.js
@@ -3,7 +3,7 @@
     .constant('CONSTS', {
       anonymousToken: 'e3365a55-86f2-4f8b-b665-95b0136ebb82',
       signUpToken: '79ede9c5-d79b-402a-b080-a987d09be510',
-      appName: 'nyc'
+      appName: 'app-nyc'
     });
 
 })();
