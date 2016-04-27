84d3b47026bc77cfc15e8ff5a0bda448335dfee6
paul (local) <ps@artspaces.net>
2016-04-27 15:28:30 -0400

    enable nodejs

--- _boot/app.sh
+++ _boot/app.sh
@@ -1,9 +1,9 @@
-#iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3080
-#cd /www/app-nyc
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3080
+cd /www/app-nyc
 # i=0;
 # while true; do
 # 	i=$[$i+1]
 # 	node app.js
 # 	sleep 6
 # done
-#node app.js
+node app.js
