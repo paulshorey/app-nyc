ffa959ee5dffae70b9806616debd07670e094d12
paul (local) <ps@artspaces.net>
2016-04-27 10:38:03 -0400

    debug

--- www/js/stamplay.js
+++ www/js/stamplay.js
@@ -1655,7 +1655,7 @@
 		}
 		a.Stamplay = a.Stamplay || {}, a.Stamplay.VERSION = "v1", a.Stamplay.APPID = "", a.Stamplay.BASEURL = "", a.Stamplay.OPTIONS = {}, window.localStorage && store.enabled && (a.Stamplay.USESTORAGE = !0), b("jwt") && a.Stamplay.USESTORAGE && store.set(window.location.origin + "-jwt", b("jwt")), a.Stamplay.init = function (b, c) {
 			a.Stamplay.BASEURL = "http://" + b + ".stamplayapp.com", a.Stamplay.APPID = b, a.Stamplay.OPTIONS = c || {}
-			a.Stamplay.OPTIONS.absoluteUrl = window.system.app.host;
+			console.log('a.Stamplay.OPTIONS',JSON.stringify(a.Stamplay.OPTIONS));
 		}
 	}(this),
 	function (a) {
