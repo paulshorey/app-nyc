4a9dec090c9f1f8d51eb26a99699b32203dad115
paul (local) <ps@artspaces.net>
2016-04-27 23:04:47 -0400

    fullchain.pem

--- app.js
+++ app.js
@@ -91,6 +91,6 @@ process.app.use(function(err, req, res, next) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // start
 var httpServer = process.http.createServer(process.app);
-var httpsServer = process.https.createServer({key: process.fs.readFileSync('/etc/letsencrypt/live/allevents.nyc/privkey.pem', 'utf8'), cert: process.fs.readFileSync('/etc/letsencrypt/live/allevents.nyc/cert.pem', 'utf8')}, process.app);
+var httpsServer = process.https.createServer({key: process.fs.readFileSync('/etc/letsencrypt/live/allevents.nyc/privkey.pem', 'utf8'), cert: process.fs.readFileSync('/etc/letsencrypt/live/allevents.nyc/fullchain.pem', 'utf8')}, process.app);
 httpServer.listen(process.env.PORT);
 httpsServer.listen(443);
