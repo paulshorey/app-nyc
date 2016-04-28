61f8200dc0b1864cd97ac6ca817a7dc1aa45146d
paul (local) <ps@artspaces.net>
2016-04-27 22:49:29 -0400

    cache?

--- www/js/app.js
+++ www/js/app.js
@@ -41,9 +41,9 @@ angular.module('ionicApp', ['ionic', 'ionicApp.controllers', 'ionicApp.services'
 		requireBase: false
 	});
 	
-	// $httpProvider.defaults.headers.post['Cache-Control'] = 'no-cache';
-	// $httpProvider.defaults.headers.post['Pragma'] = 'no-cache';
-	// $ionicConfigProvider.views.maxCache(0);
+	$httpProvider.defaults.headers.post['Cache-Control'] = 'no-cache';
+	$httpProvider.defaults.headers.post['Pragma'] = 'no-cache';
+	$ionicConfigProvider.views.maxCache(0);
 	// $httpProvider.defaults.useXDomain = true;
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
