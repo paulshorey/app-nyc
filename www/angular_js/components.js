b205ec43f65939f1129ca36be82f9dccaf4bd213
paul (local) <ps@artspaces.net>
2016-06-24 01:29:21 -0400

    loading animation in

--- www/angular_js/components.js
+++ www/angular_js/components.js
@@ -118,12 +118,12 @@ angular.module('ListModule.components', [])
 				// 	React.createElement(React.html.eventslist_loading, {data:scope.data}), 
 				// 	angular.element(element)[0]
 				// );
-				angular.element(element)[0].innerHTML = '<div className="loading-dance" style="background-image:url(\'gfx/gif/dance.gif\')}}></div>';
+				angular.element(element)[0].innerHTML = '<div class="loading-dance" style="background-image:url(\'gfx/gif/dance.gif\')"></div>';
 				$timeout(function(){
 					if (element) {
 						$(element).addClass('ready');
 					}
-				},100);
+				},500);
 			}
 
 			// lazyload
@@ -158,77 +158,77 @@ angular.module('ListModule.components', [])
 })
 
 
-.directive('reactEventslist', function (reactDirective, EventService,$timeout, $rootScope) {
-	return {
-		restrict: 'A',
-		scope: {
-			data: '='
-		},
-		link: function (scope, element, attrs) {
-			scope.vm = {};
+// .directive('reactEventslist', function (reactDirective, EventService,$timeout, $rootScope) {
+// 	return {
+// 		restrict: 'A',
+// 		scope: {
+// 			data: '='
+// 		},
+// 		link: function (scope, element, attrs) {
+// 			scope.vm = {};
 
-			// template
-			scope.list_ready = function(){
-				var query = {};
-				query.category = scope.data.category;
-				query.scene = scope.data.scene;
-				query.time = scope.data.time;
-				EventService.getEvents(query)
-				.then(function (response) {
-					scope.vm.events = response.data.data;
+// 			// template
+// 			scope.list_ready = function(){
+// 				var query = {};
+// 				query.category = scope.data.category;
+// 				query.scene = scope.data.scene;
+// 				query.time = scope.data.time;
+// 				EventService.getEvents(query)
+// 				.then(function (response) {
+// 					scope.vm.events = response.data.data;
 
-					ReactDOM.render(  
-					  React.createElement(React.html.eventslist, {vm:scope.vm,data:scope.data}), 
-					  angular.element(element)[0]
-					);
+// 					ReactDOM.render(  
+// 					  React.createElement(React.html.eventslist, {vm:scope.vm,data:scope.data}), 
+// 					  angular.element(element)[0]
+// 					);
 
-				}, function (error) {
-					console.error(error);
-				});
-			}
-			// loading
-			scope.list_reset = function(){
-				scope.vm = {};
-				ReactDOM.render( 
-					React.createElement(React.html.eventslist_loading, {data:scope.data}), 
-					angular.element(element)[0]
-				);
-				$timeout(function(){
-					if (element) {
-						$(element).addClass('ready');
-					}
-				},1000);
-			}
-			scope.list_reset();
+// 				}, function (error) {
+// 					console.error(error);
+// 				});
+// 			}
+// 			// loading
+// 			scope.list_reset = function(){
+// 				scope.vm = {};
+// 				ReactDOM.render( 
+// 					React.createElement(React.html.eventslist_loading, {data:scope.data}), 
+// 					angular.element(element)[0]
+// 				);
+// 				$timeout(function(){
+// 					if (element) {
+// 						$(element).addClass('ready');
+// 					}
+// 				},1000);
+// 			}
+// 			scope.list_reset();
 
-			// lazyload
-			if (!$rootScope.lazyLoadedLists) {
-				$rootScope.lazyLoadedLists = {};
-			}
-			scope.element = element;
-			scope.$watch(
-			function( scope ) {
-				element = scope.element;
-				var window_width = (window.innerWidth || document.documentElement.clientWidth);
-				var rect = element[0].getBoundingClientRect();
-				if (rect.left > 0 && rect.left < window_width) {
+// 			// lazyload
+// 			if (!$rootScope.lazyLoadedLists) {
+// 				$rootScope.lazyLoadedLists = {};
+// 			}
+// 			scope.element = element;
+// 			scope.$watch(
+// 			function( scope ) {
+// 				element = scope.element;
+// 				var window_width = (window.innerWidth || document.documentElement.clientWidth);
+// 				var rect = element[0].getBoundingClientRect();
+// 				if (rect.left > 0 && rect.left < window_width) {
 
-					if (!$rootScope.lazyLoadedLists[ scope.data.category ]) {
-						$rootScope.lazyLoadedLists[ scope.data.category ] = scope;
-						console.log('listing ',scope.data.category);
-						scope.list_ready()
-					}
+// 					if (!$rootScope.lazyLoadedLists[ scope.data.category ]) {
+// 						$rootScope.lazyLoadedLists[ scope.data.category ] = scope;
+// 						console.log('listing ',scope.data.category);
+// 						scope.list_ready()
+// 					}
 
-				} else {
-					if ($rootScope.lazyLoadedLists[ scope.data.category ]) {
-						delete $rootScope.lazyLoadedLists[ scope.data.category ];
-						scope.list_reset();
-					}
-				}
-			});
+// 				} else {
+// 					if ($rootScope.lazyLoadedLists[ scope.data.category ]) {
+// 						delete $rootScope.lazyLoadedLists[ scope.data.category ];
+// 						scope.list_reset();
+// 					}
+// 				}
+// 			});
 
-		}
-	}
-})
+// 		}
+// 	}
+// })
 
 ;
